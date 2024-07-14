6e10f37ec6fb667e0ff460925849a987867ae414
Aniruddhaganorkar <ganiruddha2015@gmail.com>
2024-07-14 18:24:48 +0530

    initial commit

---
+++ libs/common/src/dtos/DeleteKeyUser.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsUUID } from "class-validator";
+
+export class DeleteKeyUserDto {
+    @IsNotEmpty()
+    @IsUUID()
+    userId: string;
+
+    @IsNotEmpty()
+    @IsUUID()
+    accessKey: string;
+}
+
